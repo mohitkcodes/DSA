f3ba9f0decb6cda1bc366177fd823b15d6bf4f39
mohitkcodes <mohitvarday2006@gmail.com>
2025-09-14 18:31:00 +0500

    functiondemo

--- patterns loop/p1.cpp
+++ patterns loop/p1.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+void func() {
+    cout << "mohit";
+}
+
 int main() {
     int n = 4;
     // for(int i=0; i<n; i++) {
@@ -16,18 +20,20 @@ int main() {
     //     cout<< endl;
     // }
 
-    for(int i=0; i<n; i++) {
-        for (int j=n; j>i+1; j--) {
-            cout << " ";
-        }
+    // for(int i=0; i<n; i++) {
+    //     for (int j=n; j>i+1; j--) {
+    //         cout << " ";
+    //     }
 
-        for(int j=0; j<i+1; j++) {
-            cout << j+1;
-        }
+    //     for(int j=0; j<i+1; j++) {
+    //         cout << j+1;
+    //     }
 
-        for(int j=i; j>0; j--) {
-            cout <<j;
-        }
-        cout << endl;
-    }
+    //     for(int j=i; j>0; j--) {
+    //         cout <<j;
+    //     }
+    //     cout << endl;
+    // }
+
+    func();
 }
