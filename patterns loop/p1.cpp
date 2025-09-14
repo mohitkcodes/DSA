5d943b6661c410c12e2f1a7387da3d52d9424b9b
mohitkcodes <mohitvarday2006@gmail.com>
2025-09-14 14:55:19 +0500

    patterns

---
+++ patterns loop/p1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    // for(int i=0; i<n; i++) {
+        
+    //     for(int j=0; j<i; j++) {
+    //         cout << " ";
+    //     }
+
+    //     for(int j=n; j>i; j--) {
+    //         cout << i+1;
+    //     }
+
+    //     cout<< endl;
+    // }
+
+    for(int i=0; i<n; i++) {
+        for (int j=n; j>i+1; j--) {
+            cout << " ";
+        }
+
+        for(int j=0; j<i+1; j++) {
+            cout << j+1;
+        }
+
+        for(int j=i; j>0; j--) {
+            cout <<j;
+        }
+        cout << endl;
+    }
+}
